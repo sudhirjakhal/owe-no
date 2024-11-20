ed4a7e3c6aa82662daf9779f2b9816d29f3093b0
Gunjan Relan <mayankrelan@Gunjans-MacBook-Air.local>
2024-11-20 06:53:02 +0530

    Added Layerfile again

--- database.py
+++ database.py
@@ -3,10 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./owe_no.db"
 engine = create_engine(
-    DATABASE_URL,
-    pool_size=10,  # Default is 5
-    max_overflow=20,  # Default is 10
-    pool_timeout=30,  # Default is 30 seconds
-    pool_recycle=1800
+    DATABASE_URL
     )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
